7aefcc1d5ff0624e1af9aede7c514367e7bd6275
Colton Rogers <crrogers12@gmail.com>
2022-07-18 22:01:37 -0700

    reverse a string recursively

--- recursion/challenge6/main.go
+++ recursion/challenge6/main.go
@@ -14,12 +14,9 @@ func reverse(str string) string {
 	if len(str) <= 0 {
 		return str
 	}
-
 	newStr := strings.Split(str, "")
 	rvr := []string{}
-
 	var helper func(str1 []string) string
-
 	helper = func(str1 []string) string {
 		if len(str1) == 0 {
 			//joining the entire slice
@@ -27,10 +24,8 @@ func reverse(str string) string {
 		}
 		//adding the last index or value of the slice to rvr
 		rvr = append(rvr, str1[len(str1)-1:]...)
-
 		//removing the last letter of slice
 		return helper(str1[:len(str1)-1])
 	}
-
 	return helper(newStr)
 }
